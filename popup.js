1264b792dceaebd1300a3c2acb8ccc09635264a0
MeteorClear <tivics9973@gmail.com>
2024-11-18 19:54:10 +0900

    add feat to upload as images drop

--- popup.js
+++ popup.js
@@ -4,3 +4,27 @@ const dropZone = document.getElementById("drop-zone");
 const optionZone = document.getElementById("option-zone");
 const fileZone = document.getElementById("file-zone");
 
+["dragenter", "dragover", "dragleave", "drop"].forEach((event) => {
+    document.addEventListener(event, (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+});
+
+dropZone.addEventListener("drop", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const files = Array.from(event.dataTransfer.files);
+    const imageFiles = files.filter((file) => file.type.startsWith("image/"));
+
+    fileZone.innerHTML = "";
+
+    imageFiles.forEach((file) => {
+        const listItem = document.createElement("div");
+        const text = document.createTextNode(file.name);
+  
+        listItem.appendChild(text);
+        fileZone.appendChild(listItem);
+    });
+});
