d86f01a228ea94a284c7222e00467a627e2c1151
MeteorClear <tivics9973@gmail.com>
2024-11-18 21:05:24 +0900

    add feat to convert files and download

--- content.js
+++ content.js
@@ -8,3 +8,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
     (document.head || document.body).appendChild(scriptTop);
 });
+
+async function decodeHEIF(file) {
+    // some magic - decode
+    return;
+}
+
+async function encodeHEIF(file) {
+    // some magic - encode
+    return;
+}
+
+function downloadFile(file) {
+    // some magic - file download
+    return;
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "convertFiles") {
+        const files = message.files;
+        console.log(files);
+
+        files.forEach(async (file) => {
+            const extension = file.name.toLowerCase().slice(file.name.lastIndexOf("."));
+
+            if ([".jpeg", ".jpg", ".png"].includes(extension)) {
+                encodeHEIF(file);
+                // some magic
+            } else if ([".heif", ".heic"].includes(extension)) {
+                decodeHEIF(file);
+                // some magic
+            } else {
+                console.error("unknown extension", extension);
+            }
+
+            downloadFile(file);
+        });
+
+        sendResponse({ success: true });
+        return true;
+    }
+});
