73248e62ce35993d0a10c08cc86baa3294c1999b
MeteorClear <tivics9973@gmail.com>
2024-11-18 19:38:40 +0900

    set basic design

--- popup.js
+++ popup.js
@@ -1,2 +1,6 @@
 console.log("[TEST]:: popup.js load");
 
+const dropZone = document.getElementById("drop-zone");
+const optionZone = document.getElementById("option-zone");
+const fileZone = document.getElementById("file-zone");
+
