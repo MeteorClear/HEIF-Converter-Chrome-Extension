e8e937cd89b7eece9ab5c406140b8402a0a78e2c
MeteorClear <tivics9973@gmail.com>
2024-11-18 19:14:04 +0900

    remove unused code

--- content.js
+++ content.js
@@ -1,13 +1,10 @@
 console.log("[TEST]:: content.js load");
 
-
 const scriptSrc = chrome.runtime.getURL('lib/bundle.js');
-console.log(scriptSrc);
 
 document.addEventListener("DOMContentLoaded", () => {
     const scriptTop = document.createElement('script');
     scriptTop.src = scriptSrc;
 
-    // Append the script to the <head> if it exists, otherwise to <body>
     (document.head || document.body).appendChild(scriptTop);
 });
